cddfaec507e44110e0e9900a0175788ffcd7abe4
mansi sharma <mash02@betssongroup.com>
2020-09-02 20:57:23 +0530

    uploading files for website

---
+++ script.js
@@ -0,0 +1,26 @@
+function uploadFileToS3(){
+  var event = window.event || callmymethod.caller.arguments[0];
+  event.preventDefault ? event.preventDefault() : (event.returnValue = false);
+
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "text/*");
+  
+  var file = document.getElementById("uploadFile").files[0]
+  var fileName = document.getElementById("uploadFile").files.item(0).name
+  var results = document.getElementById('results');
+
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: file,
+    redirect: 'follow'
+  };
+  
+  fetch(`https://s4mqxcm6dj.execute-api.ap-south-1.amazonaws.com/v1/upload-file-to-s3?name=${fileName}`, requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+    results.innerHTML = 'Upload Successful';
+}
+
